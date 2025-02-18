21ff4f45190d0e65887bd72915430fae8d156cb6
Jey-Vaninah <hei.vaninah@gmail.com>
2025-02-18 20:17:05 +0100

    refactor: App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { ProfileCard } from "./assets/composant/ProfileCard"
+import { ProfileCard } from "./components/ProfileCard"
+const users = [
+  {
+    avatar: "idk",
+    name: "RickaPrincy",
+    age: 22,
+    job: "Développeur mobile",
+  },
+  {
+    avatar: "idk",
+    name: "Jenifère Vaninah",
+    age: 21,
+    job: "Développeur web",
+  },
+  {
+    avatar: "idk",
+    name: "Nomena Fitahiana",
+    age: 22,
+    job: "Backend",
+  },
+];
 export const App = () => {
-  const users = [
-    {
-      avatar: "idk",
-      name: "RickaPrincy",
-      age: 22,
-      job: "Développeur mobile",
-    },
-    {
-      avatar: "idk",
-      name: "Jenifère Vaninah",
-      age: 21,
-      job: "Développeur web",
-    },
-    {
-      avatar: "idk",
-      name: "Nomena Fitahiana",
-      age: 22,
-      job: "Backend",
-    },
-  ];
+
 
   return (
     <div>
